95fc7ed4f694d594a76a44a0458e9a4a13604438
kevin <kevinG>
2026-03-24 18:20:23 -0500

    se cambiao la importacion erronea de show y se corigio a tiempo

--- Main.py
+++ Main.py
@@ -6,6 +6,6 @@ menu()
 #------ Add_product imports are performed
 from Add_product import add
 #------ Show_inventory imports are performed
-from Show_Inventory import show_inventory
+from Show_Inventory import show
 #------ Search_inventory imports are performed
 from search_inventory import search
