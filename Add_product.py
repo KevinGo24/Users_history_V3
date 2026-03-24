3de97a83dade7f4c0da4bc28aa4c608425390a2d
kevinGo <kevinGo@gmail.com>
2026-03-24 14:46:33 -0500

    se hizo add_product para agragar los prodcutos

---
+++ Add_product.py
@@ -0,0 +1,17 @@
+
+def add_product(inventory):
+    Add = 'yes'
+    while Add == 'yes':
+        # Request the data
+        name = input("Product Name: ")
+        price = float(input("Price: "))
+        quantity = int(input("Quantity: "))
+
+        # Create the dictionary and save it in the list
+        product = {"name": name, "price": price, "quantity": quantity}
+        inventory.append(product)
+
+        print(f"{name} added successfully!")
+
+        # Ask if they want to continue adding in this same session
+        Add = input("Do you want to add another product? (yes/no): ").lower()
