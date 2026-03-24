1688615bcb2dd798e5f300f707a0bcc5fe335891
kevinGo <kevinGo@gmail.com>
2026-03-24 14:47:25 -0500

    se hizo Show_prodcut para mostar los prodcutos del inventario

---
+++ Show_Inventory.py
@@ -0,0 +1,18 @@
+from Add_product import *
+
+def show_inventory(inventory):
+
+    if not inventory:
+        print('The inventory is empty!')
+    else:
+        print("\n" + "="*30)
+        print("CURRENT INVENTORY")
+        print("="*30)
+
+# Iterate through the list of products
+
+    for product in inventory:
+        print(f"Product: {product['name']}")
+        print(f"Price: ${product['price']}")
+        print(f"quantity: {product['quantity']}")
+        print("=" * 30)
