cd38a6e57183b978460508ba33b1346cdee91f83
kevinGo <kevinGo@gmail.com>
2026-03-25 12:06:45 -0500

    se hizo la funcion para guardar cada producto (observacion)

--- Archive.py
+++ Archive.py
@@ -1,11 +1,18 @@
 import csv
-import os
 
-Name_archive = 'archive.csv'
-fields = ['product', 'price', 'quantity']
+def save_csv(inventory_list):
+    with open('inventory.csv', 'w', newline='', encoding='utf-8') as file:
+        if not inventory_list:
+            return
+        # Usamos las llaves del primer diccionario como encabezados
+        writer = csv.DictWriter(file, fieldnames=inventory_list[0].keys())
+        writer.writeheader()
+        writer.writerows(inventory_list)
+    print("¡Datos guardados en inventory.csv!")
 
-def Load():
-    if not os.path.exists(Name_archive):
-        return[]
-    with open(Name_archive, mode='r',encoding='utf-8') as x:
-        return list(csv.DictReader(x))
+def load_csv():
+    try:
+        with open('inventory.csv', 'r', encoding='utf-8') as file:
+            return list(csv.DictReader(file))
+    except FileNotFoundError:
+        return [] # Si no existe el archivo, devuelve lista vacía
