58d8b686c853165a0ddd575ce97cf7a4ca3d8c30
kevinGo <kevinGo@gmail.com>
2026-03-25 13:28:19 -0500

    Se agrego importaciones

--- Main.py
+++ Main.py
@@ -13,3 +13,8 @@ from search_inventory import search
 from Uptade_inventory import uptade
 #------ Archive.py - Archive.csv imports are performed
 from Archive import save_csv, load_csv
+#------ Inventory_Statitics imports are performed
+from inventory_statistics import buscarmaximo
+#------ Delete_Inventory imports are performed
+from Delete_inventory import  delete
+# ----------------------------------
